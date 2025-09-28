1cda86576e3745b84bd957d30903fa1f01641e95
Chandru <chandru89new@gmail.com>
2025-09-28 11:56:05 +0530

    Guarantee that SortedArray is actually sorted.

--- src/StockProfits.purs
+++ src/StockProfits.purs
@@ -2,70 +2,36 @@ module StockProfits where
 
 import Prelude
 
-import Data.Array (concat, head, snoc, sortBy, tail, reverse, length)
+import Data.Array as Array
 import Data.Maybe (Maybe(..), fromMaybe)
-import Data.Tuple (Tuple(..), fst)
+import Data.Tuple (Tuple(..))
 import Effect (Effect)
 import Effect.Console (log)
+import Types (BestCandidate, BuySell(..), SortedArray, StockDay(..), buySellProfit, head, mkArraySorted, mkReverseSortedArray, tail, (??))
 
 main :: Effect Unit
 main = do
   log "🍝"
 
-type Price = Int
-
-type Day = Int
-
-type StockDay = Tuple Price Day
-
-data BuySell = BuySell (StockDay) (StockDay)
-
-type BestCandidate = Tuple BuySell (Maybe BuySell)
-
-newtype SortedArray a = SortedArray (Array a)
-
--- derive newtype instance showSortedArrayInt :: Show (SortedArray Int)
--- derive newtype instance showSortedArrayStockDay :: Show (SortedArray StockDay)
--- derive newtype instance showSortedArrayTradePair :: Show (SortedArray BuySell)
-
-instance showTradePair :: Show BuySell where
-  show (BuySell a b) = "BuySell " <> show a <> " " <> show b
-
-instance showSortedArray :: Show a => Show (SortedArray a) where
-  show (SortedArray a) = "SortedArray " <> show a
-
-bestBuySellPair :: BuySell -> SortedArray (BuySell) -> Int -> Maybe BestCandidate -> Maybe BestCandidate
-bestBuySellPair buySell (SortedArray []) maxProfitSoFar bestTradeSoFar =
-  if buySellProfit buySell > maxProfitSoFar then (Just $ Tuple buySell Nothing)
-  else bestTradeSoFar
-bestBuySellPair buySell (SortedArray tradePairs) maxProfitSoFar bestTradeSoFar =
+bestBuySellPair :: BuySell -> SortedArray BuySell -> Int -> Maybe BestCandidate -> Maybe BestCandidate
+bestBuySellPair buySell tradePairs maxProfitSoFar bestTradeSoFar =
   case head tradePairs of
     Just h ->
-      if ((buySell ?? h) && (buySellProfit buySell + buySellProfit h) > maxProfitSoFar) then bestBuySellPair buySell (fromMaybe (SortedArray []) (map SortedArray $ tail tradePairs)) (buySellProfit buySell + buySellProfit h) (Just $ Tuple buySell (Just h))
-      else bestBuySellPair buySell (fromMaybe (SortedArray []) (map SortedArray $ tail tradePairs)) maxProfitSoFar bestTradeSoFar
+      if ((buySell ?? h) && (buySellProfit buySell + buySellProfit h) > maxProfitSoFar) then bestBuySellPair buySell (fromMaybe (mkArraySorted []) (tail tradePairs)) (buySellProfit buySell + buySellProfit h) (Just $ Tuple buySell (Just h))
+      else bestBuySellPair buySell (fromMaybe (mkArraySorted []) (tail tradePairs)) maxProfitSoFar bestTradeSoFar
     Nothing ->
       if buySellProfit buySell > maxProfitSoFar then (Just $ Tuple buySell Nothing)
       else bestTradeSoFar
-
-buySellProfit :: BuySell -> Int
-buySellProfit (BuySell a b) = fst b - fst a
-
-getFirst :: BuySell -> StockDay
-getFirst (BuySell a _) = a
-
-getSecond :: BuySell -> StockDay
-getSecond (BuySell _ b) = b
 
 workThroughStockDays :: SortedArray BuySell -> Maybe BestCandidate
 workThroughStockDays tradePairs = go tradePairs 0 (Nothing)
   where
   go :: SortedArray BuySell -> Int -> Maybe BestCandidate -> Maybe BestCandidate
-  go (SortedArray []) _ candidate = candidate
-  go (SortedArray tps) maxProfitSoFar candidate =
+  go tps maxProfitSoFar candidate =
     case head tps of
       Just tp ->
         let
-          candidate1 = bestBuySellPair tp (SortedArray tps) maxProfitSoFar candidate
+          candidate1 = bestBuySellPair tp tps maxProfitSoFar candidate
           bestCandidate = case candidate, candidate1 of
             Just c1, Just c2 -> Just $ getBestCandidate c1 c2
             Just c1, Nothing -> Just c1
@@ -73,7 +39,7 @@ workThroughStockDays tradePairs = go tradePairs 0 (Nothing)
             _, _ -> Nothing
           newMaxProfitSoFar = map candidateProfit bestCandidate # fromMaybe 0
         in
-          go (fromMaybe (SortedArray []) $ map SortedArray $ tail tps) newMaxProfitSoFar bestCandidate
+          go (fromMaybe (mkArraySorted []) $ tail tps) newMaxProfitSoFar bestCandidate
       Nothing -> candidate
 
 getBestCandidate :: BestCandidate -> BestCandidate -> BestCandidate
@@ -90,36 +56,35 @@ arrayToStockDay xs = sortStockDay $ go xs 1 []
   go :: Array Int -> Int -> Array StockDay -> Array StockDay
   go [] _ acc = acc
   go ys day acc =
-    case head ys of
-      Just stockPrice -> go (fromMaybe [] $ tail ys) (day + 1) (snoc acc (Tuple stockPrice day))
+    case Array.head ys of
+      Just stockPrice -> go (fromMaybe [] $ Array.tail ys) (day + 1) (Array.snoc acc (StockDay stockPrice day))
       Nothing -> acc
 
   sortStockDay :: Array StockDay -> SortedArray StockDay
-  sortStockDay ls = SortedArray $ sortBy (\sdp1 sdp2 -> if fst sdp1 < fst sdp2 then LT else GT) ls
+  sortStockDay = mkArraySorted
 
 makeBuySell :: StockDay -> StockDay -> Maybe BuySell
-makeBuySell t1@(Tuple p1 _) t2@(Tuple p2 _) =
+makeBuySell t1@(StockDay p1 _) t2@(StockDay p2 _) =
   if (t1 ?? t2) && p1 < p2 then Just (BuySell t1 t2) else Nothing
 
 makeBuySellList :: StockDay -> SortedArray StockDay -> Array BuySell
-makeBuySellList sdp (SortedArray xs) = go sdp xs []
+makeBuySellList sdp xs = go sdp xs []
   where
-  go _ [] acc = acc
+  go :: StockDay -> SortedArray StockDay -> Array BuySell -> Array BuySell
   go _sdp ls acc = case head ls of
     Just h -> case makeBuySell _sdp h of
-      Nothing -> go _sdp (fromMaybe [] $ tail ls) acc
-      Just tp -> go _sdp (fromMaybe [] $ tail ls) (snoc acc tp)
+      Nothing -> go _sdp (fromMaybe (mkArraySorted []) $ tail ls) acc
+      Just tp -> go _sdp (fromMaybe (mkArraySorted []) $ tail ls) (Array.snoc acc tp)
     Nothing -> acc
 
 makeBuySellCombinationsList :: SortedArray StockDay -> SortedArray BuySell
-makeBuySellCombinationsList xs = SortedArray $ reverse $ sortBy sortBuySell $ go xs ([])
+makeBuySellCombinationsList xs = mkReverseSortedArray $ go xs ([])
   where
   go :: SortedArray StockDay -> Array BuySell -> Array BuySell
-  go (SortedArray []) tps = tps
-  go (SortedArray sdps) (tps) =
+  go sdps tps =
     case head sdps of
-      Nothing -> (tps)
-      Just h -> go (SortedArray $ fromMaybe [] $ tail sdps) (concat [ tps, makeBuySellList h (SortedArray $ fromMaybe [] $ tail sdps) ])
+      Nothing -> tps
+      Just h -> go (fromMaybe (mkArraySorted []) $ tail sdps) (Array.concat [ tps, makeBuySellList h (fromMaybe (mkArraySorted []) $ tail sdps) ])
 
 arrayToBuySellList :: Array Int -> SortedArray BuySell
 arrayToBuySellList = arrayToStockDay >>> makeBuySellCombinationsList
@@ -131,24 +96,16 @@ profitFromBestCandidate :: BestCandidate -> Int
 profitFromBestCandidate (Tuple tp1 Nothing) = buySellProfit tp1
 profitFromBestCandidate (Tuple tp1 (Just tp2)) = buySellProfit tp1 + buySellProfit tp2
 
-sortBuySell :: BuySell -> BuySell -> Ordering
-sortBuySell bs1 bs2 =
-  if buySellProfit bs1 > buySellProfit bs2 then GT else LT
-
-lengthSortedArray :: forall a. SortedArray a -> Int
-lengthSortedArray (SortedArray xs) = length xs
-
 totalProfits :: Array Int -> Int
 totalProfits = findBestCandidate >>> map profitFromBestCandidate >>> fromMaybe 0
 
-class ValidTrade a where
-  validTrade :: a -> a -> Boolean
+assert :: Boolean -> Effect Unit
+assert bool = log $ if bool then "Test passed" else "Failed"
 
-infix 1 validTrade as ??
-
-instance ValidTrade BuySell where
-  validTrade (BuySell (Tuple _ a) (Tuple _ b)) (BuySell (Tuple _ c) (Tuple _ d)) =
-    (a < b && c < d && c > b) || (c < d && a < b && d < a)
-
-instance ValidTrade StockDay where
-  validTrade (Tuple _ d1) (Tuple _ d2) = d1 < d2
+runTests :: Effect Unit
+runTests = do
+  assert $ totalProfits [ 3, 1, 0, 0, 5, 4, 1, 3 ] == 7
+  assert $ totalProfits [ 3, 1, 0, 0, 5, 4, 1 ] == 5
+  assert $ totalProfits [ 3, 1, 0, 0 ] == 0
+  assert $ totalProfits [ 3, 1, 0, 0, 1 ] == 1
+  assert $ totalProfits [ 3, 1, 0, 0, 1, 2, 3 ] == 3
---
+++ src/Types.purs
@@ -0,0 +1,104 @@
+module Types
+  ( StockDay(..)
+  , Price
+  , Day
+  , BuySell(..)
+  , BestCandidate
+  , SortedArray
+  , buySellProfit
+  , mkArraySorted
+  , getArrayFromSorted
+  , lengthSortedArray
+  , mkReverseSortedArray
+  , class ValidTrade
+  , validTrade
+  , (??)
+  , head
+  , tail
+  , concat
+  ) where
+
+import Prelude
+
+import Data.Array as Array
+import Data.Maybe (Maybe(..))
+import Data.Tuple (Tuple)
+
+type Price = Int
+
+type Day = Int
+
+data StockDay = StockDay Price Day
+
+data BuySell = BuySell (StockDay) (StockDay)
+
+data SortedArray a = SortedArray (Array a)
+
+type BestCandidate = Tuple BuySell (Maybe BuySell)
+
+instance showStockDay :: Show StockDay where
+  show (StockDay p d) = "StockDay " <> show p <> " " <> show d
+
+instance showTradePair :: Show BuySell where
+  show (BuySell a b) = "BuySell (" <> show a <> ") (" <> show b <> ")"
+
+instance eqStockDay :: Eq StockDay where
+  eq (StockDay p1 d1) (StockDay p2 d2) = p1 == p2 && d1 == d2
+
+instance eqBuySell :: Eq BuySell where
+  eq (BuySell a1 b1) (BuySell a2 b2) = a1 == a2 && b1 == b2
+
+instance ordStockDay :: Ord StockDay where
+  compare = sortStockDay
+
+instance ordBuySell :: Ord BuySell where
+  compare = sortBuySell
+
+instance showSortedArray :: Show a => Show (SortedArray a) where
+  show (SortedArray arr) = "SortedArray " <> show arr
+
+sortStockDay :: StockDay -> StockDay -> Ordering
+sortStockDay (StockDay p1 _) (StockDay p2 _) =
+  if p1 < p2 then LT else GT
+
+sortBuySell :: BuySell -> BuySell -> Ordering
+sortBuySell bs1 bs2 =
+  if buySellProfit bs1 > buySellProfit bs2 then GT else LT
+
+buySellProfit :: BuySell -> Int
+buySellProfit (BuySell (StockDay p1 _) (StockDay p2 _)) = p2 - p1
+
+mkArraySorted :: forall a. Ord a => Array a -> SortedArray a
+mkArraySorted arr = SortedArray (Array.sort arr)
+
+getArrayFromSorted :: forall a. SortedArray a -> Array a
+getArrayFromSorted (SortedArray arr) = arr
+
+lengthSortedArray :: forall a. SortedArray a -> Int
+lengthSortedArray (SortedArray xs) = Array.length xs
+
+mkReverseSortedArray :: forall a. Ord a => Array a -> SortedArray a
+mkReverseSortedArray xs = SortedArray (Array.reverse $ Array.sort xs)
+
+class ValidTrade a where
+  validTrade :: a -> a -> Boolean
+
+infix 1 validTrade as ??
+
+instance ValidTrade BuySell where
+  validTrade (BuySell s1 s2) (BuySell s3 s4) =
+    validTrade s1 s2 && validTrade s2 s3 && validTrade s3 s4
+
+instance ValidTrade StockDay where
+  validTrade (StockDay _ d1) (StockDay _ d2) = d1 < d2
+
+head :: forall a. Ord a => SortedArray a -> Maybe a
+head (SortedArray xs) = Array.head xs
+
+tail :: forall a. Ord a => SortedArray a -> Maybe (SortedArray a)
+tail (SortedArray xs) = case Array.tail xs of
+  Just ys -> Just (SortedArray ys)
+  Nothing -> Nothing
+
+concat :: forall a. Ord a => SortedArray a -> SortedArray a -> SortedArray a
+concat (SortedArray xs) (SortedArray ys) = mkArraySorted (Array.concat [ xs, ys ])
